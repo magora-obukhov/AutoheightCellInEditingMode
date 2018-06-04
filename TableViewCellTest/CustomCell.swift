7dddc5f233ddc6a7372bd70cae404c67c0db85fc
Denis Obukhov <obukhov@magora-systems.com>
2018-06-04 14:45:06 +0700

    Trying to control global height constraint

--- TableViewCellTest/CustomCell.swift
+++ TableViewCellTest/CustomCell.swift
@@ -2,37 +2,38 @@
 import UIKit
 
 class CustomCell: UITableViewCell {
+    var heightConstraint: NSLayoutConstraint?
     
     private lazy var stackView: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
-        stack.distribution = .fill
+        stack.distribution = .fillEqually
         return stack
     }()
     
     private lazy var redLabel: UILabel = {
         let label = UILabel()
         label.text = "RED LABEL"
-        label.heightAnchor.constraint(equalToConstant: 100).isActive = true
         label.backgroundColor = .red
         return label
     }()
     
     private lazy var blueLabel: UILabel = {
-        let targetHeight: CGFloat = 100
+
         let label = UILabel()
         label.text = "BLUE LABEL"
         label.backgroundColor = .blue
-        //Spring constraints for resizing while animations
-        let weakConstraint = label.heightAnchor.constraint(equalToConstant: targetHeight)
-        weakConstraint.priority = .defaultHigh
-        weakConstraint.isActive = true
-        label.heightAnchor.constraint(lessThanOrEqualToConstant: targetHeight).isActive = true
+
         return label
     }()
 
     override func setEditing(_ editing: Bool, animated: Bool) {
         self.blueLabel.isHidden = !editing
+        if editing {
+            self.heightConstraint?.constant = 400
+        } else {
+            self.heightConstraint?.constant = 100
+        }
     }
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -46,6 +47,8 @@ class CustomCell: UITableViewCell {
     }
     
     private func setupView() {
+        self.heightConstraint = self.contentView.heightAnchor.constraint(equalToConstant: 100)
+        self.heightConstraint?.isActive = true
         self.contentView.fillWith(view: self.stackView)
         self.stackView.addArrangedSubview(self.redLabel)
         self.stackView.addArrangedSubview(self.blueLabel)
